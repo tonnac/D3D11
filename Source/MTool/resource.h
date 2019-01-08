9c414e4f13ab14d4f0c15545b451071484d5b4f1
tonnac <tonnac35@gmail.com>
2019-01-08 13:26:42 +0900

    dd

---
+++ Source/MTool/resource.h
@@ -0,0 +1,106 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// MTool.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_POPUP_EDIT                  119
+#define ID_STATUSBAR_PANE1              120
+#define ID_STATUSBAR_PANE2              121
+#define IDS_STATUS_PANE1                122
+#define IDS_STATUS_PANE2                123
+#define IDS_TOOLBAR_STANDARD            124
+#define IDS_TOOLBAR_CUSTOMIZE           125
+#define ID_VIEW_CUSTOMIZE               126
+#define IDR_MAINFRAME                   128
+#define IDR_MAINFRAME_256               129
+#define IDR_MToolTYPE                   130
+#define IDR_THEME_MENU                  200
+#define ID_SET_STYLE                    201
+#define ID_VIEW_APPLOOK_WIN_2000        205
+#define ID_VIEW_APPLOOK_OFF_XP          206
+#define ID_VIEW_APPLOOK_WIN_XP          207
+#define ID_VIEW_APPLOOK_OFF_2003        208
+#define ID_VIEW_APPLOOK_VS_2005         209
+#define ID_VIEW_APPLOOK_VS_2008         210
+#define ID_VIEW_APPLOOK_OFF_2007_BLUE   215
+#define ID_VIEW_APPLOOK_OFF_2007_BLACK  216
+#define ID_VIEW_APPLOOK_OFF_2007_SILVER 217
+#define ID_VIEW_APPLOOK_OFF_2007_AQUA   218
+#define ID_VIEW_APPLOOK_WINDOWS_7       219
+#define IDS_EDIT_MENU                   306
+#define IDD_DIALOG1                     310
+#define IDD_DeDlg                       311
+#define IDD_DIALOG2                     312
+#define IDD_EffectForm                  313
+#define IDD_LightForm                   314
+#define IDD_ADD_LIGHT                   319
+#define IDC_RADIO1                      1000
+#define IDC_RADIO2                      1001
+#define IDC_RADIO3                      1002
+#define IDC_EDIT1                       1004
+#define IDC_EDIT2                       1005
+#define IDC_BUTTON1                     1006
+#define IDC_POSCLOCKWISE                1006
+#define IDC_POSRCLOCKWISE               1007
+#define IDC_YPOS                        1008
+#define IDC_ZPOS                        1009
+#define IDC_YPOS2                       1010
+#define IDC_ZPOS2                       1011
+#define IDC_YPOS3                       1012
+#define IDC_ZPOS3                       1013
+#define IDC_YPOS4                       1014
+#define IDC_ZPOS4                       1015
+#define IDC_ZPOS5                       1016
+#define IDC_DIRROTSPEED                 1016
+#define IDC_ZPOS6                       1017
+#define IDC_POSROTSPEED                 1017
+#define IDC_FALL_END                    1018
+#define IDC_SPOTFACTOR                  1019
+#define IDC_GSTRENGTH                   1020
+#define IDC_BSTRENGTH                   1021
+#define IDC_COMBO2                      1026
+#define IDC_LightList                   1026
+#define IDC_BUTTON3                     1028
+#define IDC_BUTTON4                     1029
+#define IDC_BUTTON5                     1030
+#define IDC_COMBO1                      1034
+#define IDC_LIGHT_TYPE                  1035
+#define IDC_LIGHT_POS                   1036
+#define IDC_XPOS                        1037
+#define IDC_LIGHT_DIR                   1038
+#define IDC_XPOS2                       1039
+#define IDC_XPOS3                       1040
+#define IDC_DIRROT                      1041
+#define IDC_CHECK2                      1042
+#define IDC_CHECK3                      1043
+#define IDC_XPOS4                       1044
+#define IDC_STATIC11                    1045
+#define IDC_STATIC12                    1046
+#define IDC_RSTRENGTH                   1047
+#define IDC_FALL_START                  1048
+#define IDC_DIRCLOCKWISE                2000
+#define IDC_DIRRCLOCKWISE               2001
+#define ID_TOOL_MTOOL                   32771
+#define ID_TOOL_MTOOL1                  32772
+#define ID_TOOL_MTOOL2                  32773
+#define ID_TOOL_32774                   32774
+#define ID_TOOL_32775                   32775
+#define ID_CREATE_CH                    32776
+#define ID_LOAD_CH                      32777
+#define ID_SAVE_CH                      32778
+#define ID_SKY_BOX                      32782
+#define ID_BACK_COLOR                   32783
+#define ID_32784                        32784
+#define ID_WIREFRAME                    32785
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        321
+#define _APS_NEXT_COMMAND_VALUE         32790
+#define _APS_NEXT_CONTROL_VALUE         1049
+#define _APS_NEXT_SYMED_VALUE           315
+#endif
+#endif
